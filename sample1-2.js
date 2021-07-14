57948b1457b85650828b60372dcc5cd82319718d
Kim Taesik <goodtaesik@gmail.com>
2021-07-14 13:03:34 +0900

    changes 2021-07-14
    Changes contents

---
+++ sample1-2.js
@@ -0,0 +1,12 @@
+Function myFunc1(){
+console.log('This is s example for javascript function');
+}
+
+myFunc1();
+
+function myFunc1(){
+const str = 'This s a example for javascript Function';
+console.log(str);
+}
+
+myFunc1();
